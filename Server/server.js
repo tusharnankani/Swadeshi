d7189c40557d21880827f2e6b590af689c3f7aaa
Kartik Soneji <kartiksoneji@rocketmail.com>
2020-07-02 19:04:19 +0530

    Setup static paths.

--- Server/server.js
+++ Server/server.js
@@ -1,42 +1,37 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
 
 const app = express();
 
 // DB connection
-const db = 'mongodb://127.0.0.1:27017/tsec_codestorm_july_2020'
-mongoose.connect
-(
-    db, 
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }
-).then
-(
-    () => console.log('MongoDB local connected...')
-).catch
-(
-    (err) => console.log(err)
-);
+const db = "mongodb://127.0.0.1:27017/tsec_codestorm_july_2020"
+mongoose.connect(
+	db, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	})
+	.then(() => console.log("MongoDB local connected..."))
+	.catch(console.err);
 
 // Middleware
 app.use(express.urlencoded({extended: false}));
 
-//Routes
-app.use('/wholesaler', require('./routes/wholesaler.js'));
-app.use('/farmer', require('./routes/farmer.js'));
+// Routes
+//app.use("/wholesaler", require("./routes/wholesaler.js"));
+//app.use("/farmer", require("./routes/farmer.js"));
+
+// Static files
+app.use(express.static(path.join(__dirname, "../HomePage")));
 
 // Port
 const PORT = process.env.PORT || 5000;
-app.listen
-(
-    PORT,
-    (err) =>
-    {
-        if(err)
-            throw err;
+app.listen(
+	PORT,
+	(err) => {
+		if(err)
+			throw err;
 
-        console.log(`Server started on PORT ${PORT}...`);
-    }
+		console.log("Server started on http://localhost:" + PORT);
+	}
 );
