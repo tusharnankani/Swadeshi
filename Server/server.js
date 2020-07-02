b75dd6452b1e3d5b7a40e88930013818f1732fea
Harsh Kapadia <harshgkapadia@gmail.com>
2020-07-02 19:34:02 +0530

    Move static above routes in `server.js`

--- Server/server.js
+++ Server/server.js
@@ -5,24 +5,24 @@ const path = require("path");
 const app = express();
 
 // DB connection
-const db = "mongodb://127.0.0.1:27017/tsec_codestorm_july_2020"
+const db = require('./config/keys').MongoURI;
 mongoose.connect(
 	db, {
 		useNewUrlParser: true,
 		useUnifiedTopology: true
 	})
-	.then(() => console.log("MongoDB local connected..."))
+	.then(() => console.log("MongoDB connected..."))
 	.catch(console.err);
 
 // Middleware
 app.use(express.urlencoded({extended: false}));
 
+// Static files
+app.use(express.static(path.join(__dirname, "../HomePage")));
+
 // Routes
 //app.use("/wholesaler", require("./routes/wholesaler.js"));
 //app.use("/farmer", require("./routes/farmer.js"));
-
-// Static files
-app.use(express.static(path.join(__dirname, "../HomePage")));
 
 // Port
 const PORT = process.env.PORT || 5000;
