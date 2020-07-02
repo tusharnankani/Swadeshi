ae91e6c26977bbf915082297b8b5acdf5821d579
Harsh Kapadia <harshgkapadia@gmail.com>
2020-07-02 16:05:42 +0530

    Add `mongoose` & set up `server.js`

---
+++ Server/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// DB connection
+const db = 'mongodb://127.0.0.1:27017/tsec_codestorm_july_2020'
+mongoose.connect
+(
+    db, 
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+).then
+(
+    () => console.log('MongoDB local connected...')
+).catch
+(
+    (err) => console.log(err)
+);
+
+// Middleware
+app.use(express.urlencoded({extended: false}));
+
+// Port
+const PORT = process.env.PORT || 5000;
+app.listen
+(
+    PORT,
+    (err) =>
+    {
+        if(err)
+            throw err;
+
+        console.log(`Server started on PORT ${PORT}...`);
+    }
+);
