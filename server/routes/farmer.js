fe05c8be55fb84d5f223e7a4ca28e10e267b43a8
Harsh Kapadia <harshgkapadia@gmail.com>
2020-07-04 18:53:24 +0530

    Complete `farmer` routes

--- server/routes/farmer.js
+++ server/routes/farmer.js
@@ -6,90 +6,110 @@ const router = express.Router();
 // Include model
 const User = require("../models/User.js");
 
+// To retrieve phone number if authenticated
+const util = require("../util.js");
+
 router.get
 (
 	"/products",
-	(req, res) =>
+	async (req, res) =>
 	{
-		User.find
-		(
-			{
-				id: ObjectId(placeholder)
-			},
-		).then
-		(
-			(result) =>
-			{
-				res.status(200).send({ "products": result.products });
-			}
-		).catch
-		(
-			(err) => console.log(err)
-		);
+		const ph_no = await util.authenticateUser(req);
+
+		if(ph_no != null)
+		{
+			User.find
+			(
+				{
+					id: ph_no
+				},
+			).then
+			(
+				(result) =>
+				{
+					res.status(200).send({ "products": result.products });
+				}
+			).catch
+			(
+				(err) => console.log(err)
+			);
+		}
+		else
+			res.status(401).send({ "message": "User not authenticated" });
 	}
 );
 
 router.post
 (
 	"/product/add",
-	(req, res) =>
+	async (req, res) =>
 	{
-		// Doubt: If we need to insert stuff like this, the User model needs to be modified.
-		const new_product = {
-			category: req.body.category,
-			item: req.body.item,
-			orig_qty: req.body.orig_qty,
-			avail_qty: req.body.avail_qty,
-			price: req.body.price
-		}
+		const ph_no = await util.authenticateUser(req);
 
-		// Doubt: Will the phone no. have to be accessed from the auth token sent by the browser and then put below in the update query?
-		const placeholder = 0;
-
-		User.updateOne
-		(
-			{
-				_id: ObjectId(placeholder)
-			},
-			{
-				$push: {
-					products: new_product
-				}
+		if(ph_no != null)
+		{
+			const new_product = {
+				category: req.body.category,
+				item: req.body.item,
+				orig_qty: req.body.orig_qty,
+				avail_qty: req.body.avail_qty,
+				price: req.body.price
 			}
-		).then
-		(
-			(result) => res.status(200).send("Product added")
-		).catch
-		(
-			(err) => console.log(err)
-		);
+	
+			User.updateOne
+			(
+				{
+					id: ph_no
+				},
+				{
+					$push: {
+						products: new_product
+					}
+				}
+			).then
+			(
+				(result) => res.status(200).send("Product added")
+			).catch
+			(
+				(err) => console.log(err)
+			);
+		}
+		else
+			res.status(401).send({ "message": "User not authenticated" });
 	}
 );
 
 router.delete
 (
 	"/product/remove",
-	(req, res) =>
+	async (req, res) =>
 	{
-		User.updateOne
-		(
-			{
-				_id: ObjectId(placeholder)
-			},
-			{
-				$pull: {
-					products: {
-						item: req.body.item
+		const ph_no = await util.authenticateUser(req);
+
+		if(ph_no != null)
+		{
+			User.updateOne
+			(
+				{
+					id: ph_no
+				},
+				{
+					$pull: {
+						products: {
+							item: req.body.item
+						}
 					}
 				}
-			}
-		).then
-		(
-			res.send(200).send({ "message": "Item deleted" })
-		).catch
-		(
-			(err) => console.log(err)
-		);
+			).then
+			(
+				res.send(200).send({ "message": "Item deleted" })
+			).catch
+			(
+				(err) => console.log(err)
+			);
+		}
+		else
+			res.status(401).send({ "message": "User not authenticated" });
 	}
 );
 
